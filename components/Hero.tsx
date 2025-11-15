c8decdd684852d2a21e845a74ee421d57da70362
MD. BAKIBILLAH RAHAT <mbr@MDs-MacBook-Pro.local>
2025-11-15 17:25:06 +0600

    fix some issue

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,13 +1,10 @@
 import React from 'react'
-import { Spotlight } from './ui/Spotlight'
 
 const Hero = () => {
   return (
     <div className='pb-20 pt-36'>
         <div>
-            <Spotlight className='-top-40 -left-10 md:-left-32 md:-top-2- h-screen fill="white' fill='white'/>
-            <Spotlight className='top-10 left-full h-[80vh] w-[50vw]' fill='purple'/>
-            <Spotlight className='top-28 left-80 h-[80vh] w-[50vw]' fill='blue'/>
+            <h1 className='text-5xl md:text-7xl font-bold text-white mb-6'>Welcome to My Portfolio</h1>
         </div>
     </div>
   )
