6c3ed97b892df3c1d147ee6172f35f725461babf
MD. BAKIBILLAH RAHAT <mbr@MDs-MacBook-Pro.local>
2025-11-25 21:54:32 +0600

    fix some error

--- app/page.tsx
+++ app/page.tsx
@@ -62,7 +62,7 @@ export default function Home() {
   return (
     <div className="relative min-h-screen flex justify-center items-center bg-[#0a0a0a] text-white p-4 md:py-14 font-mono overflow-hidden">
       <h1 className="text-white text-8xl font-bold">
-        STARTER CODE
+        MD. BAKIBILLAH RAHAT | Professional Software Developer
       </h1>
     </div>
   );
