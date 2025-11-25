e2243c84d5bb47943b75f269dc05b1c826ccf7db
MD. BAKIBILLAH RAHAT <mbr@MDs-MacBook-Pro.local>
2025-11-25 21:45:53 +0600

    add some tag line

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,6 +5,7 @@ const Hero = () => {
     <div className='pb-20 pt-36'>
         <div>
             <h1 className='text-5xl md:text-7xl font-bold text-white mb-6'>Welcome to My Portfolio</h1>
+            <h2>I am MD. Bakibillah Rahat A <strong>Professional Software Developer </strong></h2>
         </div>
     </div>
   )
